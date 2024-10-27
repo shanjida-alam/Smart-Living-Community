f35edd398ce84da3bb75b0acfaa788d18c516e20
Hasneen Tamanna Totinee <jucse29.362@gmail.com>
2024-10-27 22:52:51 +0600

    initial commit from totinee-registration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.google.android.material:material:1.4.0") // for form input fields
+
 }
